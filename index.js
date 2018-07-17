3155e10c71c23db27fefa74252cc7129176fb309
Xmader <xmader@outlook.com>
2018-07-17 17:54:08 +0800

    膜法指南增加 `逗`

--- index.js
+++ index.js
@@ -15,17 +15,16 @@ const init_modal = (key, a) => {
     $("#m_unformatted_body")[0].value = json[key]["contents"][a]
 }
 
-const init_video_modal = (src, title) => {
+const init_video_img_modal = (src, title, type) => {
     $("#m_title").text(title)
-    $("#m_body").html(`<video src="${src}" preload="Metadata" controls></video>`)
+    $("#m_body").html(type == "img" ? `<img src="${src}" />` : `<video src="${src}" preload="Metadata" controls></video>`)
     $("#copy").hide()
-    if (is_Firefox) {
-        $(".download_video").remove()
-        $(".modal-footer").prepend(`<a href="${src}" target="_blank" class="btn btn-primary download_video">下载视频</a>`)
-    }
+
+    $(".download_video").remove()
+    $(".modal-footer").prepend(`<a href="${src}" target="_blank" class="btn btn-primary download_video" download>下载${type == "img" ? "图片" : "视频"}</a>`)
 }
 
-// const is_electron_app = navigator.userAgent.indexOf("Electron") > -1
+var is_electron_app = navigator.userAgent.indexOf("Electron") > -1
 const is_Firefox = navigator.userAgent.indexOf("Firefox") > -1;
 const is_Chrome = navigator.userAgent.indexOf("Chrome") > -1;
 
@@ -39,14 +38,14 @@ $.get("https://mohu.oss-cn-shanghai.aliyuncs.com/" + t + ".json", (data) => {
     json = data
     var keys = _.keys(data)
     var card_deck = $("#card-deck")
-    if (t == "chang" || t == "videos") {
+    if (t == "dou" || t == "chang" || t == "videos") {
         keys.shift()
     }
 
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i]
 
-        if (t == "chang" || t == "videos") {
+        if (t == "dou" || t == "chang" || t == "videos") {
             var items = _.keys(data[key])
         } else {
             var items = data[key]["titles"]
@@ -56,15 +55,20 @@ $.get("https://mohu.oss-cn-shanghai.aliyuncs.com/" + t + ".json", (data) => {
 
 
         for (var a = 0; a < items.length; a++) {
-            if (t == "chang") {
-                item_html += `<li class="list-group-item grey">${data[key][items[a]]}<a href="${data["url"]}${items[a]}.mp3" target="_blank" class="download_music"><i class="fa fa-download" aria-hidden="true"></i></a><audio class="audio${is_Firefox ? "_Firefox" : ""}" src="${data["url"]}${items[a]}.mp3" controls>
-                </audio></li>`
-            }
-            else {
-                if (t == "videos") {
-                    item_html += `<li class="list-group-item grey"><a data-toggle="modal" href="#modal" data-target="#modal" onclick="init_video_modal('${json["url"]}${items[a]}','${data[key][items[a]]}');">${data[key][items[a]]}</a></li>`
+            switch (t) {
+                case "dou": {
+                    item_html += `<li class="list-group-item grey"><a data-toggle="modal" href="#modal" data-target="#modal" onclick="init_video_img_modal('${json["url"]}${items[a]}','${data[key][items[a]]}','img');">${data[key][items[a]]}</a></li>`
+                    break;
                 }
-                else {
+                case "chang": {
+                    item_html += `<li class="list-group-item grey">${data[key][items[a]]}<a href="${data["url"]}${items[a]}.mp3" target="_blank" class="download_music" download><i class="fa fa-download" aria-hidden="true"></i></a><audio class="audio${is_Firefox ? "_Firefox" : ""}" src="${data["url"]}${items[a]}.mp3" controls></audio></li>`
+                    break;
+                }
+                case "videos": {
+                    item_html += `<li class="list-group-item grey"><a data-toggle="modal" href="#modal" data-target="#modal" onclick="init_video_img_modal('${json["url"]}${items[a]}','${data[key][items[a]]}');">${data[key][items[a]]}</a></li>`
+                    break;
+                }
+                default: {
                     item_html += `<li class="list-group-item"><a data-toggle="modal" href="#modal" data-target="#modal" onclick="init_modal('${key}',${a});">${items[a]}</a></li>`
                 }
             }
