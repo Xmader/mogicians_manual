2ff58aa566275db0f25f6854c56ea21d6b3bac1e
xmader <xmader@outlook.com>
2018-09-12 18:06:41 +0800

    底部导航条Vue组件优化

--- components/bottom-nav.js
+++ components/bottom-nav.js
@@ -9,31 +9,16 @@
  * 
 */
 
-Vue.component('nav-item', {
-    props: ["id", "icon", "name"],
-    template: `
-        <li class="nav-item">
-            <a :class="['nav-link',{ active: isActive }]" :href="'#/'+id" :id="id" @click="active()">
-                <i :class="['fa','fa-'+icon]" aria-hidden="true"></i> &nbsp;{{name}}
-            </a>
-        </li>
-    `,
-    computed: {
-        isActive: function () {
-            return (location.hash.slice(2) || "shuo") == this.id
-        }
-    },
-    methods: {
-        active: function () {
-            // this.isActive = (location.hash.slice(2) || "shuo") == this.id
-            // 底部导航条高亮当前子页面
-            $(".nav-link").removeClass('active')
-            $("#" + this.id).addClass('active')
-        }
-    }
-})
-
 Vue.component('bottom-nav', {
+    template: `
+        <ul :class="bottom_nav" id="bottom-nav">
+            <li class="nav-item" v-for="nav of navs">
+                <a :class="['nav-link',{ active: Active_Item == nav.id }]" :href="'#/'+nav.id" :id="nav.id" @click="active(nav.id)">
+                    <i :class="['fa','fa-'+nav.icon]" aria-hidden="true"></i> &nbsp;{{nav.name}}
+                </a>
+            </li>
+        </ul>
+    `,
     data: () => ({
         "bottom_nav": ["nav", "nav-pills", "nav-fill", "bottom-nav"],
         "Active_Item": (location.hash.slice(2) || "shuo"),
@@ -65,18 +50,9 @@ Vue.component('bottom-nav', {
             }
         ]
     }),
-    template: `
-        <ul :class="bottom_nav" id="bottom-nav">
-            <li class="nav-item" v-for="nav of navs">
-                <a :class="['nav-link',{ active: Active_Item == nav.id }]" :href="'#/'+nav.id" :id="nav.id" @click="active(nav.id)">
-                    <i :class="['fa','fa-'+nav.icon]" aria-hidden="true"></i> &nbsp;{{nav.name}}
-                </a>
-            </li>
-        </ul>
-    `,
     methods: {
         active: function (id) {
-            this.Active_Item =  id
+            this.Active_Item = id
         }
     }
 })
