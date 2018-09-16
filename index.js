d5d108915657877618decbb6371a25a5cc15d279
Xmader <xmader@outlook.com>
2018-09-16 22:12:29 +0800

    保存获取的资源到sessionStorage,优化性能

--- index.js
+++ index.js
@@ -34,17 +34,26 @@ const init = () => { // 初始化页面
         }
     ]
 
-    var json_callback = vm.$refs.card_deck.json_callback
+    var json_callback = (text) => {
+        sessionStorage.setItem(sub_page_name, text); // 保存获取的资源到sessionStorage, 加快下一次访问此子页面的加载速度, 优化性能
+        vm.$refs.card_deck.json_callback(text)
+    }
 
-    // 获取资源文件
-    if (offline) {
-        // 强行解决Firefox中不能访问本地资源的问题, 不保证长期有效
-        var json_element = document.createElement("script")
-        json_element.src = `resource/${sub_page_name}.json?callback=json_callback`
-        document.getElementsByTagName("body")[0].appendChild(json_element)
+    if (sessionStorage.getItem(sub_page_name)) { // 从sessionStorage获取资源, 避免多次重复读取资源文件拖慢加载速度
+        var text = sessionStorage.getItem(sub_page_name);
+        vm.$refs.card_deck.json_callback(text)
     }
     else {
-        make_get_request("https://raw.githubusercontent.com/Xmader/mogicians_manual/offline/resource/" + sub_page_name + ".json", json_callback, '获取资源失败!\n')
+        // 获取资源文件
+        if (offline) {
+            // 强行解决Firefox中不能访问本地资源的问题, 不保证长期有效
+            var json_element = document.createElement("script")
+            json_element.src = `resource/${sub_page_name}.json?callback=json_callback`
+            document.getElementsByTagName("body")[0].appendChild(json_element)
+        }
+        else {
+            make_get_request("https://raw.githubusercontent.com/Xmader/mogicians_manual/offline/resource/" + sub_page_name + ".json", json_callback, '获取资源失败!\n')
+        }
     }
 }
 
